e61e845cc05b95b6de783f7a52f02173da0554b8
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-08-13 10:39:24 +0600

    feat:36-3 Installing Zod And Creating First Zod Schema For Validation

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express, { type Application, type Request, type Response } from "express";
+import express, {
+  NextFunction,
+  type Application,
+  type Request,
+  type Response,
+} from "express";
 import httpStatus from "http-status";
 import config from "./app/config";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import { notFound } from "./app/middleware/notFound";
 import { AuthRoutes } from "./app/module/auth/auth.route";
+import z from "zod";
 
 const app: Application = express();
 
 app.use(
-	cors({
-		origin: config.frontend_url,
-		credentials: true,
-	}),
+  cors({
+    origin: config.frontend_url,
+    credentials: true,
+  }),
 );
 
 // Enable URL-encoded form data parsing
@@ -23,14 +29,49 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", AuthRoutes);
+app.use("/api/v1/auth", AuthRoutes);
+
+app.post("/zod", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const UserZodSchema = z.object({
+      name: z.string(),
+      age: z.number(),
+      isVerified: z.boolean().optional(),
+      book: z.array(z.string()).optional(),
+    });
+
+    const payload = req.body;
+    // const result = UserZodSchema.parse(payload);
+	const result = UserZodSchema.safeParse(payload);
+
+	if(!result.error){
+		console.log(result.error);
+	}
+	if(result.success){
+		console.log(result.data);
+	}
+
+	console.log(result);
+
+	
+
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "Welcome to PH Healthcare System Backend",
+	  data:result 
+    });
+  } catch (error) {
+	console.log(error);
+    next(error);
+  }
+});
 
 // Basic route
 app.get("/", async (req: Request, res: Response) => {
-	res.status(httpStatus.OK).json({
-		success: true,
-		message: "Welcome to PH Healthcare System Backend",
-	});
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Welcome to PH Healthcare System Backend",
+  });
 });
 
 app.use(globalErrorHandler);
