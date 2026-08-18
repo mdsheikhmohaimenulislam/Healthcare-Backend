d2547be01fad248c666fb0e10242ce0503384acd
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-08-18 12:01:41 +0600

    38-7 Configuring Cloudinary And Multer

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -31,4 +31,7 @@ export default {
   SMTP_PASSWORD: process.env.SMTP_PASSWORD!,
   SMTP_USER: process.env.SMTP_USER!,
   email_sender: process.env.EMAIL_SENDER!,
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY!,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET!,
 };
