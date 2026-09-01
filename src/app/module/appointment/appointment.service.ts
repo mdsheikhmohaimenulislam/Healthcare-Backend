34e7c7dda6ff5d6457cd6f31d5ea789aefa50a1e
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-09-01 16:44:00 +0600

    42-10 Creating APIs For Updating Appointment Status

--- src/app/module/appointment/appointment.service.ts
+++ src/app/module/appointment/appointment.service.ts
@@ -163,7 +163,7 @@ const bookAppointment = async (payload: IBookAppointmentPayload , user: RequestU
 	return transactionResult;
 };
 
- const payAppointment = async (payload: IPayAppointmentPayload, user: RequestUser) => {
+const payAppointment = async (payload: IPayAppointmentPayload, user: RequestUser) => {
 	const appointmentId = payload.appointmentId;
 
 	const existingAppointment = await prisma.apppointment.findUnique({
@@ -469,8 +469,6 @@ const bookAppointmentCallback = async (query: Record<string, any>) => {
 	return transactionResult;
 };
 
-
-// Cancel Appointment.....
 const cancelAppointment = async (payload: ICancelAppointmentPayload, user : RequestUser) => {
 	const transactionResult = await prisma.$transaction(async (tx) => {
 		const appointmentId = payload.appointmentId;
