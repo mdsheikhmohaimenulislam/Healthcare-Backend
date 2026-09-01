8a37566f1048df5438e4162bf8435b5a6d25583d
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-09-02 05:58:34 +0600

    42-12 Creating APIs For Getting My Payments, Getting All Payments And Getting Single Payment

--- src/app.ts
+++ src/app.ts
@@ -46,8 +46,8 @@ app.use("/api/v1/appointment", AppointementRoutes);
 app.use("/api/v1/doctor", DoctorRoutes);
 app.use("/api/v1/schedule", ScheduleRoutes);
 app.use("/api/v1/payment",PaymentRoutes);
-// app.use("/api/v1/prescription", PrescriptionRoutes);
-app.use("/api/v1/analytics", AnalyticsRoutes);
+app.use("/api/v1/prescription", PrescriptionRoutes);
+// app.use("/api/v1/analytics", AnalyticsRoutes);
 
 app.get("/test", async (req: Request, res: Response, next: NextFunction) => {
 	try {
